3f1734ce2c4d182bd89d652787d1d18bad04f188
Phạm Quang Trường <truongpham090325@gmail.com>
2025-10-29 17:55:27 +0700

    2.5. Backend lưu dữ liệu vào cơ sở dữ liệu

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -1,10 +1,29 @@
 import { Request, Response } from "express";
+import AccountUser from "../model/accounts-user.model";
+import bcrypt from "bcryptjs";
 
-export const registerPost = (req: Request, res: Response) => {
-  console.log(req.body);
+export const registerPost = async (req: Request, res: Response) => {
+  const existAccount = await AccountUser.findOne({
+    email: req.body.email,
+  });
+
+  if (existAccount) {
+    res.json({
+      code: "error",
+      message: "Email đã tồn tại trong hệ thống!",
+    });
+    return;
+  }
+
+  //Mã hóa mật khẩu
+  const salt = await bcrypt.genSalt(10);
+  req.body.password = bcrypt.hashSync(req.body.password, salt);
+
+  const newAccount = new AccountUser(req.body);
+  await newAccount.save();
 
   res.json({
     code: "success",
-    messgae: "Đăng ký tài khoản thành công",
+    message: "Đăng ký tài khoản thành công!",
   });
 };
