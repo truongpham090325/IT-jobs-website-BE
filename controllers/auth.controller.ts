73018473738ea7f249a9e43d58fe4f4083d683cc
Phạm Quang Trường <truongpham090325@gmail.com>
2025-11-01 09:16:58 +0700

    03. Tính năng Đăng nhập (Nhà tuyển dụng)

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import AccountUser from "../models/accounts-user.model";
+import AccountCompany from "../models/accounts-company.model";
 
 export const check = async (req: Request, res: Response) => {
   try {
@@ -20,31 +21,55 @@ export const check = async (req: Request, res: Response) => {
     ) as jwt.JwtPayload;
     const { id, email } = decoded;
 
-    const existAccount = await AccountUser.findOne({
+    // Tìm user
+    const existAccountUser = await AccountUser.findOne({
       _id: id,
       email: email,
     });
 
-    if (!existAccount) {
+    if (existAccountUser) {
+      const infoUser = {
+        id: existAccountUser.id,
+        fullName: existAccountUser.fullName,
+        email: existAccountUser.email,
+      };
+
+      res.json({
+        code: "success",
+        message: "Token hợp lệ!",
+        infoUser: infoUser,
+      });
+      return;
+    }
+
+    // Tìm company
+    const existAccountCompany = await AccountCompany.findOne({
+      _id: id,
+      email: email,
+    });
+
+    if (existAccountCompany) {
+      const infoCompany = {
+        id: existAccountCompany.id,
+        companyName: existAccountCompany.companyName,
+        email: existAccountCompany.email,
+      };
+
+      res.json({
+        code: "success",
+        message: "Token hợp lệ!",
+        infoCompany: infoCompany,
+      });
+      return;
+    }
+
+    if (!existAccountCompany && !existAccountUser) {
       res.clearCookie("token");
       res.json({
         code: "error",
         message: "Token không hợp lệ!",
       });
-      return;
     }
-
-    const infoUser = {
-      id: existAccount.id,
-      fullName: existAccount.fullName,
-      email: existAccount.email,
-    };
-
-    res.json({
-      code: "success",
-      message: "Token hợp lệ!",
-      infoUser: infoUser,
-    });
   } catch (error) {
     res.clearCookie("token");
     res.json({
