09d76fba49deef9aee90394a5cf3410dabc2eef2
Phạm Quang Trường <truongpham090325@gmail.com>
2025-11-04 09:06:01 +0700

    01. Tích hợp bộ soạn thảo TinyMCE

--- routes/index.route.ts
+++ routes/index.route.ts
@@ -3,6 +3,7 @@ import userRoutes from "./user.route";
 import authRoutes from "./auth.route";
 import companyRoutes from "./company.route";
 import cityRoutes from "./city.route";
+import uploadRoutes from "./upload.route";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use("/user", userRoutes);
 router.use("/auth", authRoutes);
 router.use("/company", companyRoutes);
 router.use("/city", cityRoutes);
+router.use("/upload", uploadRoutes);
 
 export default router;
---
+++ routes/upload.route.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import * as uploadController from "../controllers/upload.controller";
+import multer from "multer";
+import { storage } from "../helpers/clouldinary.helper";
+
+const router = Router();
+
+const upload = multer({ storage: storage });
+
+router.post("/image", upload.single("file"), uploadController.imagePost);
+
+export default router;
