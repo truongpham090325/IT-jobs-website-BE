ea26f7bca8ce93e260b023ac3bd749cf87569cad
Phạm Quang Trường <truongpham090325@gmail.com>
2025-11-06 15:07:22 +0700

    1.1. Hiển thị data theo trường language

--- routes/index.route.ts
+++ routes/index.route.ts
@@ -4,6 +4,7 @@ import authRoutes from "./auth.route";
 import companyRoutes from "./company.route";
 import cityRoutes from "./city.route";
 import uploadRoutes from "./upload.route";
+import searchRoutes from "./search.route";
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.use("/auth", authRoutes);
 router.use("/company", companyRoutes);
 router.use("/city", cityRoutes);
 router.use("/upload", uploadRoutes);
+router.use("/search", searchRoutes);
 
 export default router;
